99409a6c7e903512a5029788d5546387aa002510
Bobby Hines <bobby@conflabs.com>
2018-02-22 22:45:30 -0800

    Unit test stub for App\lib\Disable\Disable class. File path assertion complete.
    Signed-off-by: Bobby Hines <bobby@conflabs.com>

---
+++ tests/unit/Disable/DisableClassTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Disable;
+
+class DisableClassTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testFilePath()
+    {
+        $filePath = $_SERVER['DOCUMENT_ROOT'] . 'lib/Disable/Disable.php';
+        $this->assertFileExists($filePath);
+    }
+}
